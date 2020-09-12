8e17231bdfac84674c94ac6c4ad05ddf86f9f99d
Alex Leung <alex.leung@outlook.com>
2020-09-12 13:40:27 -0400

    Initial commit, including start.spring.io boilerplate.

---
+++ learning-spring/src/main/java/ca/alexleung/lil/learningspring/LearningSpringApplication.java
@@ -0,0 +1,13 @@
+package ca.alexleung.lil.learningspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSpringApplication.class, args);
+	}
+
+}
---
+++ learning-spring/src/test/java/ca/alexleung/lil/learningspring/LearningSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.alexleung.lil.learningspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
