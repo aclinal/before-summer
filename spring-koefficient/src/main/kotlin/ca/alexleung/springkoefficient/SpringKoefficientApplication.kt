c38d8824b897d861931c65ff9df53f269f37cc04
Alex Leung <aleung@squareup.com>
2020-09-14 21:50:43 -0400

    Initial commit of spring-koefficient

---
+++ spring-koefficient/src/main/kotlin/ca/alexleung/springkoefficient/SpringKoefficientApplication.kt
@@ -0,0 +1,11 @@
+package ca.alexleung.springkoefficient
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringKoefficientApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringKoefficientApplication>(*args)
+}
