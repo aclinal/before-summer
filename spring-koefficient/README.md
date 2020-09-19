db5eec4df01d146b6bf1463585a7334d683df308
Alex Leung <aleung@squareup.com>
2020-09-19 17:52:34 -0400

    Add a Kotlin RestController example.

---
+++ spring-koefficient/README.md
@@ -0,0 +1,15 @@
+# Spring Koefficient
+
+LinkedIn Learning: Spring Boot Essentials, using Kotlin and Gradle.
+
+## Building
+
+From the root directory:
+
+`./gradlew clean bootJar`
+
+## Running
+
+After building:
+
+`java -jar ./build/libs/spring-koefficient-0.0.1-SNAPSHOT.jar`
--- spring-koefficient/src/main/kotlin/ca/alexleung/springkoefficient/SpringKoefficientApplication.kt
+++ spring-koefficient/src/main/kotlin/ca/alexleung/springkoefficient/SpringKoefficientApplication.kt
@@ -2,10 +2,22 @@ package ca.alexleung.springkoefficient
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class SpringKoefficientApplication
 
+@RestController
+@RequestMapping("/api")
+class ApiController {
+  @GetMapping("/greeting")
+  fun getGreeting(): String {
+    return "Hello world from API."
+  }
+}
+
 fun main(args: Array<String>) {
-	runApplication<SpringKoefficientApplication>(*args)
+  runApplication<SpringKoefficientApplication>(*args)
 }
